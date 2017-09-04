f7bcc496e3dd5e5b314845ae083b964dbc706b2f
Landon GB <landogbland@gmail.com>
2017-09-03 18:46:31 -0600

    Select via id query

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,23 @@
 module Main where
 
-import Models.Category
-import Queries.Category
+import           Models.Category
+import           Queries.Category
+import           Control.Monad
+import           Data.ConfigFile
+import           Data.Either.Utils
+import           Database.PostgreSQL.Simple
+import           Text.Printf
+import qualified Data.ByteString.Char8 as BS
 
-import Control.Monad
-import Data.ConfigFile
-import Data.Either.Utils
-import Database.PostgreSQL.Simple
-import Text.Printf
-import qualified Data.ByteString.Char8 as C
-
-parseDbString :: ConfigParser -> Either CPError C.ByteString
+parseDbString :: ConfigParser -> Either CPError BS.ByteString
 parseDbString cp = liftM5 buildDbString (getDb "host") (getDb "port")
                                         (getDb "username") (getDb "password")
                                         (getDb "dbname")
   where
     getDb = get cp "POSTGRES"
 
-buildDbString :: String -> String -> String -> String -> String -> C.ByteString
-buildDbString host port user password dbname = C.pack connStr
+buildDbString :: String -> String -> String -> String -> String -> BS.ByteString
+buildDbString host port user password dbname = BS.pack connStr
   where
     connTemplate = "host=%s port=%s user=%s password=%s dbname=%s"
     connStr      = printf connTemplate host port user password dbname
@@ -35,4 +34,10 @@ main = do
     categories <- runCategoryQuery connection categoriesQuery
     case null categories of
         True  -> putStrLn "No categories exist in the table"
+        False -> mapM_ print categories
+
+    let catId = 2
+    category <- runCategoryQuery connection $ categoryByIdQuery catId
+    case null category of
+        True  -> putStrLn $ printf "Category id %d does not exist in the table" catId
         False -> mapM_ print categories
--- src/Queries/Category.hs
+++ src/Queries/Category.hs
@@ -4,7 +4,8 @@ module Queries.Category where
 
 import           Models.Category
 
-import           Opaleye (queryTable, Query, runQuery, Column, PGInt4, PGText)
+import           Opaleye (queryTable, Query, runQuery, Column, PGInt4, PGText,
+                          restrict, (.==), pgInt4)
 import           Control.Arrow (returnA)
 import qualified Database.PostgreSQL.Simple as PGS
 
@@ -12,6 +13,11 @@ import qualified Database.PostgreSQL.Simple as PGS
 categoriesQuery :: Query CategoryColumn
 categoriesQuery = queryTable categoryTable
 
--- TODO move somewhere else. Generalize?
+categoryByIdQuery :: Int -> Query CategoryColumn
+categoryByIdQuery id = proc () -> do
+                           category <- categoriesQuery -< ()
+                           restrict -< categoryId category .== pgInt4 id
+                           returnA  -< category
+
 runCategoryQuery :: PGS.Connection -> Query CategoryColumn -> IO [Category]
 runCategoryQuery = runQuery
