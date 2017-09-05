3a7d6f4fdaa3adfb63be72e173b39e8254797780
Landon GB <landogbland@gmail.com>
2017-09-05 11:11:54 -0600

    Query specific categories initial commit

--- src/RestAPI/Category.hs
+++ src/RestAPI/Category.hs
@@ -18,15 +18,23 @@ import           Data.Either.Utils (forceEither)
 import           Database.PostgreSQL.Simple (connectPostgreSQL)
 import qualified Network.Wai as NW
 import qualified Servant as S
-import           Servant ((:>))
+import           Servant ((:>), (:<|>)(..))
 
 type CategoryApi = "categories" :> S.Get '[S.JSON] [CategoryRead]
+              :<|> "categories" :> S.Capture "categoryId" Int :> S.Get '[S.JSON] CategoryRead
 
+categoryApi :: S.Proxy CategoryApi
+categoryApi = S.Proxy
 
 -- TODO get a connection pool for postgres connections instead of making
 --      one per request. This is just a dummy setup to test things work.
+--
 categoryServer :: S.Server CategoryApi
-categoryServer = do
+categoryServer = getAllCategories
+            :<|> getCategoryById
+
+getAllCategories :: S.Handler [CategoryRead]
+getAllCategories = do
     cp <- liftIO getConfigParser
     let connStr = forceEither $ parseDbString cp
     connection <- liftIO $ connectPostgreSQL connStr
@@ -34,8 +42,18 @@ categoryServer = do
     return categories
 
 
-categoryApi :: S.Proxy CategoryApi
-categoryApi = S.Proxy
+-- TODO return 404 with error if category doesn't exist
+-- TODO this query should just return a Maybe with the Just being the single
+--      result fo the query, instead of returning a list and making us deal
+--      with it
+getCategoryById :: Int -> S.Handler CategoryRead
+getCategoryById categoryId = do
+    cp <- liftIO getConfigParser
+    let connStr = forceEither $ parseDbString cp
+    connection <- liftIO $ connectPostgreSQL connStr
+    category <- liftIO $ runCategoryQuery connection $ categoryByIdQuery categoryId
+    return $ category !! 0
+
 
 app :: NW.Application
 app = S.serve categoryApi categoryServer
