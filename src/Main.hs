213b65b8ad084e2e3b6b2055806414cc95981515
Landon GB <landogbland@gmail.com>
2017-09-08 16:11:00 -1000

    Pass postgres connection around to endpoints via ReaderT monad

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,17 @@
 module Main where
 
---import Models.Category
---import Queries.Category
+import ConfigHelper
 import RestAPI.Category
---import ConfigHelper
 
+import Data.Either.Utils (forceEither)
+import Database.PostgreSQL.Simple (connectPostgreSQL, Connection)
 import Network.Wai.Handler.Warp (run)
---import Data.Either.Utils
---import Database.PostgreSQL.Simple
---import Text.Printf
 
-main :: IO ()
-main = run 8081 app
+-- TODO hlint everything
 
-{-
 main :: IO ()
 main = do
     cp <- getConfigParser
     let connStr = forceEither $ parseDbString cp
     connection <- connectPostgreSQL connStr
-
-    categories <- runCategoryQuery connection categoriesQuery
-    case null categories of
-        True  -> putStrLn "No categories exist in the table"
-        False -> mapM_ print categories
-
-    let catId = 2
-    category <- runCategoryQuery connection $ categoryByIdQuery catId
-    case null category of
-        True  -> putStrLn $ printf "Category id %d does not exist in the table" catId
-        False -> mapM_ print category
-
-    -- TODO get better day then rows added back from this call
-    -- TODO catch exception when unique constraint fails
-    rowsAdded <- categoryInsert connection $ categoryToPG . makeCategory $ "Funny"
-    putStrLn $ show rowsAdded
--}
+    run 8080 $ app connection
--- src/RestAPI/Category.hs
+++ src/RestAPI/Category.hs
@@ -9,50 +9,62 @@
 
 module RestAPI.Category where
 
-import           ConfigHelper
 import           Models.Category
 import           Queries.Category
 
 import           Control.Monad.IO.Class (liftIO)
-import           Data.Either.Utils (forceEither)
-import           Database.PostgreSQL.Simple (connectPostgreSQL)
+import           Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
+import           Control.Monad.Trans.Except (ExceptT)
+import           Database.PostgreSQL.Simple (Connection)
 import qualified Network.Wai as NW
+import qualified Opaleye as O
 import qualified Servant as S
-import           Servant ((:>), (:<|>)(..))
+import           Servant ((:>), (:<|>) (..), (:~>))
+
+-- TODO break everything that will need to go into multiple apis up into
+--      a different file (like RestHandler, etc)
+
 
 type CategoryApi = "categories" :> S.Get '[S.JSON] [CategoryRead]
               :<|> "categories" :> S.Capture "categoryId" Int :> S.Get '[S.JSON] CategoryRead
 
+-- TODO get a connection pool instead of single connection in the ReaderT monad
+type RestHandler = ReaderT Connection S.Handler
+
 categoryApi :: S.Proxy CategoryApi
 categoryApi = S.Proxy
 
--- TODO get a connection pool for postgres connections instead of making
---      one per request. This is just a dummy setup to test things work.
---
-categoryServer :: S.Server CategoryApi
+categoryServer :: S.ServerT CategoryApi RestHandler
 categoryServer = getAllCategories
-            :<|> getCategoryById
-
-getAllCategories :: S.Handler [CategoryRead]
-getAllCategories = do
-    cp <- liftIO getConfigParser
-    let connStr = forceEither $ parseDbString cp
-    connection <- liftIO $ connectPostgreSQL connStr
-    categories <- liftIO $ runCategoryQuery connection categoriesQuery
-    return categories
-
--- TODO return json if category is not found
-getCategoryById :: Int -> S.Handler CategoryRead
-getCategoryById categoryId = do
-    cp <- liftIO getConfigParser
-    let connStr = forceEither $ parseDbString cp
-    connection <- liftIO $ connectPostgreSQL connStr
-    category <- liftIO $ categoryById connection categoryId
-    case category of
-        Just c  -> return c
-        Nothing -> S.throwError categoryNotFound
+             :<|> getCategoryById
   where
-    categoryNotFound = S.err404 { S.errBody = "cagegoryId not found (TODO RETURN JSON" }
+    getAllCategories :: RestHandler [CategoryRead]
+    getAllCategories = do
+        connection <- ask
+        liftIO $ O.runQuery connection categoriesQuery
 
-app :: NW.Application
-app = S.serve categoryApi categoryServer
+    -- TODO return json if category is not found
+    -- TODO what happens if I change this to return Maybe CategoryRead? If
+    --      `Nothing` is the result, what does the resulting JSON look like?
+    --      Could I do more transformers here so that I didn't have to evaulate
+    --      case of maybe, and just do '<-' instead? Could I still have that
+    --      return a 404?
+    -- TODO run query here instead of cateboryById, and do another function to
+    --      verify that exactly one result is found (like listToMaybe_
+    getCategoryById :: Int -> RestHandler CategoryRead
+    getCategoryById categoryId = do
+        connection <- ask
+        category <- liftIO $ categoryById connection categoryId
+        case category of
+            Just c  -> return c
+            Nothing -> S.throwError categoryNotFound
+      where
+        categoryNotFound = S.err404 { S.errBody = "cagegoryId not found (TODO RETURN JSON" }
+
+-- TODO I understand why this is needed, but I don't understand how it
+--      actually works at all.
+readerTToExcept :: Connection -> RestHandler :~> S.Handler
+readerTToExcept connection = S.NT (\r -> runReaderT r connection)
+
+app ::Connection -> NW.Application
+app conn = S.serve categoryApi $ S.enter (readerTToExcept conn) categoryServer
