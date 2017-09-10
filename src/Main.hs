39238996437ea8d1945baa168cb2b95ef1ad1ff2
Landon GB <landogbland@gmail.com>
2017-09-09 16:27:21 -1000

    Add connection pool for db connections

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,22 @@
 module Main where
 
-import ConfigHelper
-import RestAPI.Category
+import           ConfigHelper
+import           RestAPI.Category
 
-import Data.Either.Utils (forceEither)
-import Database.PostgreSQL.Simple (connectPostgreSQL, Connection)
-import Network.Wai.Handler.Warp (run)
+import           Data.Either.Utils (forceEither)
+import           Data.Pool (createPool)
+import qualified Database.PostgreSQL.Simple as PGS
+import           Network.Wai.Handler.Warp (run)
 
 -- TODO hlint everything
 -- TODO add logging (WIA middleware + reader)
+-- TODO change pool timeout and whatnot info to config file (and increase
+--      timeout of resource in the pool to match postgres config, bigger is
+--      probably better)
 
 main :: IO ()
 main = do
     cp <- getConfigParser
     let connStr = forceEither $ parseDbString cp
-    connection <- connectPostgreSQL connStr
-    run 8080 $ app connection
+    pool <- createPool (PGS.connectPostgreSQL connStr) PGS.close 1 (5*60) 5
+    run 8080 $ app pool
--- src/RestAPI/Category.hs
+++ src/RestAPI/Category.hs
@@ -17,6 +17,7 @@ import           Control.Monad.IO.Class (liftIO)
 import           Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
 import           Control.Monad.Trans.Except (ExceptT)
 import           Control.Monad.Trans.Class (lift)
+import           Data.Pool (Pool, withResource)
 import           Database.PostgreSQL.Simple (Connection)
 import qualified Network.Wai as NW
 import qualified Opaleye as O
@@ -35,8 +36,7 @@ type CategoryApi = "categories" :>
                 :<|> S.Capture "id" Int :> S.DeleteNoContent '[S.JSON] S.NoContent
                      )
 
--- TODO get a connection pool instead of single connection in the ReaderT monad
-type RestHandler = ReaderT Connection S.Handler
+type RestHandler = ReaderT (Pool Connection) S.Handler
 
 categoryApi :: S.Proxy CategoryApi
 categoryApi = S.Proxy
@@ -48,18 +48,21 @@ categoryServer = getAllCategories
   where
     getAllCategories :: RestHandler [CategoryRead]
     getAllCategories = do
-        conn <- ask
+        pool <- ask
+        conn <- getConnFromPool pool
         liftIO $ O.runQuery conn categoriesQuery
 
     getCategoryById :: Int -> RestHandler CategoryRead
     getCategoryById categoryId = do
-        conn     <- ask
+        pool <- ask
+        conn <- getConnFromPool pool
         category <- liftIO $ O.runQuery conn $ categoryByIdQuery categoryId
         lift $ oneOrError category $ categoryNotFound categoryId
 
     deleteCategoryById :: Int -> RestHandler S.NoContent
     deleteCategoryById id = do
-        conn <- ask
+        pool <- ask
+        conn <- getConnFromPool pool
         numDeleted <- liftIO $ O.runDelete conn categoryTable
                                (\cat -> (categoryId cat) .== (O.pgInt4 id))
         case numDeleted of
@@ -70,10 +73,11 @@ categoryNotFound :: Int -> S.ServantErr
 categoryNotFound id = errorToJSON 404 "CategoryNotFound"
                       $ "Category " ++ (show id) ++ " was not found."
 
--- TODO I understand why this is needed, but I don't understand how it
---      actually works at all.
-restHandlerToExcept :: Connection -> RestHandler :~> S.Handler
-restHandlerToExcept connection = S.NT (\r -> runReaderT r connection)
+restHandlerToSHandler :: (Pool Connection) -> RestHandler :~> S.Handler
+restHandlerToSHandler pool = S.NT (\r -> runReaderT r pool)
 
-app ::Connection -> NW.Application
-app conn = S.serve categoryApi $ S.enter (restHandlerToExcept conn) categoryServer
+getConnFromPool :: (Pool Connection) -> RestHandler Connection
+getConnFromPool pool = withResource pool return
+
+app :: (Pool Connection) -> NW.Application
+app pool = S.serve categoryApi $ S.enter (restHandlerToSHandler pool) categoryServer
