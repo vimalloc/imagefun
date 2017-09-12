d10d3eae55ed76bf35de158f19b7ccfcd5dd0b76
Landon GB <landogbland@gmail.com>
2017-09-11 21:09:29 -1000

    Minor tweaks

--- src/RestAPI/Category/Errors.hs
+++ src/RestAPI/Category/Errors.hs
@@ -6,10 +6,13 @@ import           RestAPI.RestHelpers
 import qualified Servant as S
 
 categoryNotFound :: Int -> S.ServantErr
-categoryNotFound id = encodeJSONError $ JSONError 404 "Category Not Found"
-                                        ("Category " ++ show id ++ " was not found.")
+categoryNotFound id = encodeJSONError err
+  where
+    err = JSONError 404 "Category Not Found"
+                    $ "Category " ++ show id ++ " was not found."
+
 uniqueFailedError :: CategoryWrite -> S.ServantErr
 uniqueFailedError c = encodeJSONError err
   where
     err = JSONError 409 "Cagetory Exists"
-                    ("The category '" ++ categoryName c ++ "' already exists")
+                    $ "The category '" ++ categoryName c ++ "' already exists"
